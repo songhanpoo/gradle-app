742f0aaf8be0b9caca4d506abd12c91fbf355b3b
songhanpoo <songhanpoo@Paul-desktop.localdomain>
2022-06-15 15:32:41 +0700

    debug

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,8 +1,8 @@
 jte{
-    allow_scm_jenkinsfile = false
-    permissive_initialization = true
-    // pipeline_template = "pipeline_config.groovy"
-    reverse_library_resolution = true
+    allow_scm_jenkinsfile = true
+    // permissive_initialization = true
+    // // pipeline_template = "pipeline_config.groovy"
+    // reverse_library_resolution = true
 }
 
 @merge libraries{
