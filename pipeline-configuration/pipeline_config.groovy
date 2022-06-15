7bb29d19e4a200e2535eb238652087eebcec254e
songhanpoo <songhanpoo@Paul-desktop.localdomain>
2022-06-15 10:41:56 +0700

    1st commit

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,10 +1,11 @@
 jte{
-    allow_scm_jenkinsfile = false
-    permissive_initialization = true
-    reverse_library_resolution = true
+    allow_scm_jenkinsfile = true
+    // permissive_initialization = true
+    // reverse_library_resolution = true
     // pipeline_template = "governance"
 }
 
-@merge libraries{
-    maven
-}
+// @merge libraries{
+//     maven
+//     sonarqube
+// }
