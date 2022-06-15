c7518008e29aca1575e76688bc41f2a941eac16f
songhanpoo <songhanpoo@Paul-desktop.localdomain>
2022-06-15 10:57:44 +0700

    1st commit

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -2,7 +2,7 @@ jte{
     allow_scm_jenkinsfile = false
     // permissive_initialization = true
     // reverse_library_resolution = true
-    // pipeline_template = "governance"
+    pipeline_template = "pipeline_config.groovy"
 }
 
 libraries{
