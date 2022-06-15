e18ce3dfcef07e7bea4924d3ce5e63f398f65c73
songhanpoo <songhanpoo@Paul-desktop.localdomain>
2022-06-15 10:01:28 +0700

    1st commit

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,9 +1,9 @@
 jte{
     allow_scm_jenkinsfile = false
-    pipeline_template = "governance"
+    // pipeline_template = "governance"
 }
 
 libraries{
     maven
-    sonarqube
+    // sonarqube
 }
